b2f335b7ef8e44cfbc24ededc2e35054477c62e1
DeigoPaulo <deigo.paulo@gmail.com>
2026-01-17 16:48:44 -0300

    Arrumei o negocio de senha

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,12 @@ DATABASES = {
 # ==============================================================================
 # 6. VALIDAÇÃO DE SENHA
 # ==============================================================================
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 
 # ==============================================================================
